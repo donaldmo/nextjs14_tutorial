ed1e5698625b1019df02d0bbfcd87091533ef00a
Donald Motswiri <domotswiri@gmail.com>
2024-08-31 18:36:16 +0200

    Tutorial 10 Not Found Page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 type ReviewDetailProp = {
     params: {
         productId: string,
@@ -6,7 +8,12 @@ type ReviewDetailProp = {
 }
 
 export default function ReviewDetail({ params }: ReviewDetailProp) {
-    const { productId, reviewId } = params
+    const { productId, reviewId } = params;
+
+    if (parseInt(reviewId) > 1000) {
+        notFound();
+    }
+
     return (
         <>
             <h1>Review {productId} for product {reviewId}</h1>
